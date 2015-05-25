c899a2e65850262f72b2c339c7529bd14a6bb6bb
benarnon <benarnon@post.bgu.ac.il>
2015-05-25 15:17:21 +0100

    "1.1 appendbrothers v1.0 generateClique v1.0"

--- QuasiBiClustering/src/main/java/Enumeration/EnumarationTreeForCategories_Quasi.java
+++ QuasiBiClustering/src/main/java/Enumeration/EnumarationTreeForCategories_Quasi.java
@@ -16,7 +16,7 @@ public class EnumarationTreeForCategories_Quasi
     ArrayList<GeneUnit> _geneUnits;
 
     int _uniqueGenesInCatNum; //only different ENSG
-    int _total_num_of_genes;//unique - for p-value
+    int _total_num_of_samples;//unique - for p-value
 
     public ArrayList<Cluster> _clusters;//_cluster == good p_value
     public int _allClusters,_uniqueClusters;
@@ -37,7 +37,26 @@ public class EnumarationTreeForCategories_Quasi
     //Category _category;
 
 
-    public EnumarationTreeForCategories_Quasi(DataForEnumaration data, ArrayList samplesInCat) {
+    public EnumarationTreeForCategories_Quasi(DataForEnumaration data, ArrayList samplesInCat) throws Exception {
+        _data = data;
+
+        _samplesInCat = samplesInCat;
+        _total_num_of_samples = data.getSamples().size();
+
+        EnumarationProcess_Quasi ep = new EnumarationProcess_Quasi();
+        ep.work(_samplesInCat,_data.getGeneUnitList());
+        ArrayList<Quasi_biclique> cliques = ep._quasi_bicliques;
+
+        System.out.println("NUMBER OF CLIQUES " + cliques.size());
+
+        //Convert the bi_cliques into clusters data structure
+        ArrayList<Cluster> clusters = new ArrayList<Cluster>();
+        Quasi_biclique clique;
+        for (int i = 0; i < cliques.size(); i++) {
+
+
+
+        }
 
     }
     //Nested method need to be delete after finish testing
