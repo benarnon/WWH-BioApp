ebc8ebb5c3a93942b96b41e45739fb222e703154
benarnon <benarnon@post.bgu.ac.il>
2015-05-25 15:32:06 +0100

    "minor change on main run"

--- QuasiBiClustering/src/main/java/Mains/Main_Run.java
+++ QuasiBiClustering/src/main/java/Mains/Main_Run.java
@@ -9,7 +9,7 @@ import java.io.IOException;
  * Created by user on 5/10/15.
  */
 public class Main_Run {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         String genomesFile = EnumParams.GenomesF;
         String sampleFile = EnumParams.SamplsF;
         String matrixFile = EnumParams.MatrixF;
@@ -18,6 +18,7 @@ public class Main_Run {
         System.out.println("DONE with data for enumeration");
 
         EnumarationTreeForCategories_Quasi tree = new EnumarationTreeForCategories_Quasi(data,data.getSamples());
+        System.out.println("checking git");
 
     }
 }
